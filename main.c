46319e1c7cf86880f00d43c3b9be115eaebce1f5
djtash <okutajoshuatash256@gmail.com>
2025-08-09 20:39:58 +0300

    dfg

---
+++ main.c
@@ -0,0 +1,24 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int tash = 22;
+    int immy = 21;
+    printf("tash = %d", tash);
+    
+   int second = tash;
+    printf("\n second = %d", second);
+    printf("\n immy = %d", immy);
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    return 0;
+}
